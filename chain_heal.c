bf3b4c1d3209d52e81e67e242801aecea9019b53
Madelyn Gross <mgross10@hydra8.eecs.utk.edu>
2025-01-28 17:09:21 -0500

    okay now I have the graph set up and I think it is actually right

--- chain_heal.c
+++ chain_heal.c
@@ -4,6 +4,8 @@
 #include <unistd.h>
 #include <math.h> 
 
+//./chain_heal 2 1 4 500 0.25 < small.txt  
+
 typedef struct node{
     char *name;
     int x, y;
@@ -15,7 +17,7 @@ typedef struct node{
 
 int main(int argc, char const *argv[])
 {
-    printf("hello world\n");
+    //printf("hello world\n");
 
     if(argc != 6){
         printf("Usage: %s <initial_range> <jump_range> <num_jumps> <initial_power> <power_reduction>\n", argv[0]);
@@ -28,7 +30,7 @@ int main(int argc, char const *argv[])
     int initial_power = atoi(argv[4]);
     double power_reduction = atof(argv[5]);
 
-    printf("Initial range: %i, Jump Range: %i, Num of Jumps: %i, Initial Power: %i, Power Reduction: %f\n", initial_range, jump_range, num_jumps, initial_power, power_reduction);
+    //printf("Initial range: %i, Jump Range: %i, Num of Jumps: %i, Initial Power: %i, Power Reduction: %f\n", initial_range, jump_range, num_jumps, initial_power, power_reduction);
 
     int x, y, current_pp, max_pp;
     char name[101] = "";
@@ -47,10 +49,12 @@ int main(int argc, char const *argv[])
         player->cur_PP = current_pp;
         player->max_PP = max_pp;
 
-        player->name = malloc(strlen(name) + 1);
-        strcpy(player->name, name);
+        //player->name = malloc(strlen(name) + 1);
+        player->name = strdup(name);
 
         player->prev = prev;
+
+        player->adj_size = 0;
 
         //printf("After assignment\n");
         //printf("X-cood: %i, Y-cood: %i, current power: %i, max power: %i, name: %s \n", player->x, player->y, player->cur_PP, player->max_PP, player->name);
@@ -71,10 +75,54 @@ int main(int argc, char const *argv[])
     //     prev = prev->prev;
     // }
 
+    //putting it into the array
     for(int i = 0; i < size; i++){
         players[i] = prev;
-        printf("X-cood: %i, Y-cood: %i, current power: %i, max power: %i, name: %s \n", players[i]->x, players[i]->y, players[i]->cur_PP, players[i]->max_PP, players[i]->name);
+        //printf("X-cood: %i, Y-cood: %i, current power: %i, max power: %i, name: %s \n", players[i]->x, players[i]->y, players[i]->cur_PP, players[i]->max_PP, players[i]->name);
         prev = prev->prev;
+    }
+
+    for(int i=0; i < size; i++){
+        int x = players[i]->x;
+        int y = players[i]->y;
+
+        for(int j=0; j < size; j++){
+            if(players[j]->name != players[i]->name){
+                int xDiff = abs((players[j]->x - x));
+                int yDiff = abs((players[j]->y - y));
+
+                //printf("XDiff: %i, YDiff: %i\n", xDiff, yDiff);
+
+                int hopsNeeded = xDiff + yDiff;
+
+                if(hopsNeeded <= jump_range){
+                    //printf("Adding to size of %s\n", players[i]->name);
+                    players[i]->adj_size++;
+                }
+
+            }
+        }
+
+        players[i]->adj = malloc((players[i]->adj_size)*sizeof(Node *));
+
+        int neighborsFound = 0;
+
+        for(int j=0; j < size; j++){
+            if(players[j]->name != players[i]->name){
+                int xDiff = abs((players[j]->x - x));
+                int yDiff = abs((players[j]->y - y));
+
+                //printf("XDiff: %i, YDiff: %i\n", xDiff, yDiff);
+
+                int hopsNeeded = xDiff + yDiff;
+
+                if(hopsNeeded <= jump_range){
+                    //printf("Adding %s to adj list of %s\n",players[j]->name, players[i]->name);
+                    players[i]->adj[neighborsFound] = players[j];
+                }
+
+            }
+        }
     }
 
 
