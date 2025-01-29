17566d39a748992043654b9f26d83ba47f01b348
Madelyn Gross <mgross10@hydra8.eecs.utk.edu>
2025-01-29 11:06:24 -0500

    okay so we have the graph being created correctly and we are finding the right paths

--- chain_heal.c
+++ chain_heal.c
@@ -5,6 +5,7 @@
 #include <math.h> 
 
 //./chain_heal 2 1 4 500 0.25 < small.txt  
+//./chain_heal 1 2 4 500 0.25 < small.txt 
 
 typedef struct node{
     char *name;
@@ -14,7 +15,42 @@ typedef struct node{
     int adj_size;
     struct node **adj;
     int visited;
+    int withinInitialRange;
 } Node;
+
+typedef struct game{
+    int initial_range;
+    int jump_range;
+    int num_jumps;
+    int initial_power;
+    double power_reduction;
+    
+} Game;
+
+void DFS(Node *player, int hopNumber, Game *game){
+    //printf("Hello");
+
+    if(hopNumber > game->num_jumps){
+        //printf("Node: %s Hop %i\n", player->name, hopNumber);
+        return;
+    }
+
+    if(player == NULL || player->visited == 1){
+        //printf("Node: %s Hop %i\n", player->name, hopNumber);
+        return;
+    }
+    player->visited = 1;
+    //printf("Visiting: %s\n", player->name);
+
+    printf("Node: %s Hop %i\n", player->name, hopNumber);
+
+    for(int i = 0; i < player->adj_size; i++){
+        //printf("Calling from %s loop\n", player->name);
+        DFS(player->adj[i], hopNumber+1, game);
+    }
+
+    player->visited = 0;
+}
 
 int main(int argc, char const *argv[])
 {
@@ -24,12 +60,14 @@ int main(int argc, char const *argv[])
         printf("Usage: %s <initial_range> <jump_range> <num_jumps> <initial_power> <power_reduction>\n", argv[0]);
         return 1;
     }
+    
+    Game game;
 
-    int initial_range = atoi(argv[1]);
-    int jump_range = atoi(argv[2]);
-    int num_jumps = atoi(argv[3]);
-    int initial_power = atoi(argv[4]);
-    double power_reduction = atof(argv[5]);
+    game.initial_range = atoi(argv[1]);
+    game.jump_range = atoi(argv[2]);
+    game.num_jumps = atoi(argv[3]);
+    game.initial_power = atoi(argv[4]);
+    game.power_reduction = atof(argv[5]);
 
     //printf("Initial range: %i, Jump Range: %i, Num of Jumps: %i, Initial Power: %i, Power Reduction: %f\n", initial_range, jump_range, num_jumps, initial_power, power_reduction);
 
@@ -56,7 +94,7 @@ int main(int argc, char const *argv[])
         player->prev = prev;
 
         player->adj_size = 0;
-
+        player->visited = 0;
         //printf("After assignment\n");
         //printf("X-cood: %i, Y-cood: %i, current power: %i, max power: %i, name: %s \n", player->x, player->y, player->cur_PP, player->max_PP, player->name);
 
@@ -77,7 +115,7 @@ int main(int argc, char const *argv[])
     // }
 
     //putting it into the array
-    for(int i = 0; i < size; i++){
+    for(int i = size-1; i >= 0 ; i--){
         players[i] = prev;
         //printf("X-cood: %i, Y-cood: %i, current power: %i, max power: %i, name: %s \n", players[i]->x, players[i]->y, players[i]->cur_PP, players[i]->max_PP, players[i]->name);
         prev = prev->prev;
@@ -87,21 +125,32 @@ int main(int argc, char const *argv[])
         int x = players[i]->x;
         int y = players[i]->y;
 
+        int initialRangeSquared = game.initial_range*game.initial_range;
+
         for(int j=0; j < size; j++){
-            if(players[j]->name != players[i]->name){
-                int xDiff = abs((players[j]->x - x));
-                int yDiff = abs((players[j]->y - y));
+            //he can jump diagnally!!! YOU HAVE TO CHANGE THIS
+            int xDiff = (players[j]->x - x);
+            int yDiff = (players[j]->y - y);
 
-                //printf("XDiff: %i, YDiff: %i\n", xDiff, yDiff);
+            //printf("XDiff: %i, YDiff: %i\n", xDiff, yDiff);
 
-                int hopsNeeded = xDiff + yDiff;
+            int hopsNeeded = (xDiff*xDiff) + (yDiff*yDiff);
 
-                if(hopsNeeded <= jump_range){
-                    //printf("Adding to size of %s\n", players[i]->name);
-                    players[i]->adj_size++;
-                }
-
+            if((hopsNeeded <= (game.jump_range*game.jump_range)) && players[j]->name != players[i]->name){
+                //printf("Adding to size of %s\n", players[i]->name);
+                players[i]->adj_size++;
             }
+
+            //Urgosa_the_Healing_Shaman
+            //do something here that if initial players name matches this and 
+            //current player is within initial range you set the flag to true
+            //printf("Hops needed: %i RangeSquared: %i\n", hopsNeeded, initialRangeSquared);
+            if(hopsNeeded <= initialRangeSquared && strcmp("Urgosa_the_Healing_Shaman", players[i]->name)==0){
+                //printf("%s within range of Urgosa\n", players[j]->name);
+                players[j]->withinInitialRange = 1;
+            }
+
+            
         }
 
         players[i]->adj = malloc((players[i]->adj_size)*sizeof(Node *));
@@ -117,17 +166,37 @@ int main(int argc, char const *argv[])
 
                 int hopsNeeded = xDiff + yDiff;
 
-                if(hopsNeeded <= jump_range){
+                if(hopsNeeded <= game.jump_range){
                     //printf("Adding %s to adj list of %s\n",players[j]->name, players[i]->name);
                     players[i]->adj[neighborsFound] = players[j];
+                    neighborsFound++;
                 }
 
+                
             }
         }
     }
 
+    //just for checking
+    // for(int i = 0; i < size; i++){
+    //     printf("Name: %s Links: %i \n", players[i]->name, players[i]->adj_size);
 
+    //     for(int j = 0; j < players[i]->adj_size; j++){
+    //         Node *neighbor = players[i]->adj[j];
+    //         //printf("hello\n");
+    //         if(neighbor != NULL){
+    //             printf("  -> %s \n", neighbor->name);
+    //         }
+    //     }
+    // }
 
+    for(int i = 0; i < size; i++){
+        //printf("We are calling DFS from the beginning again\n");
+        if(players[i]->withinInitialRange == 1){
+            DFS(players[i], 1, &game);
+        }
+        
+    }
 
     return 0;
 }
