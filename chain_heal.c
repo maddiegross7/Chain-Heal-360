393468f7d03c4963ff309c4c439bf3b9b8a7619c
Madelyn Gross <mgross10@hydra8.eecs.utk.edu>
2025-02-02 13:29:49 -0500

    okay everything is good to go

--- chain_heal.c
+++ chain_heal.c
@@ -60,17 +60,22 @@ void DFS(Node *player, int hopNumber, Game *game, double total_healing, Node *fr
 
     int allHealing = 0;
 
+    //adding all of them up to see what the total score WOULD be
     Node *current = player;
     for(int i=hopNumber-1; i >=0; i--){
         allHealing += current->healing;
         current = current->prev;
     } 
 
+    //checks to see if this route is better than the current best one
     if(allHealing > game->best_healing){
+        //if it is we set all the variables to reflect this new route
         game->best_healing = allHealing;
         game->best_path_length = hopNumber;
         Node *current = player;
 
+        //setting the path into these arrays so we have individual 
+        //values for each player in the path
         for(int i=hopNumber-1; i >=0; i--){
             game->best_path[i] = current;
             game->healing[i] = current->healing;
@@ -78,6 +83,7 @@ void DFS(Node *player, int hopNumber, Game *game, double total_healing, Node *fr
         }        
     }
 
+    //reducing the healing power and calling DFS again
     double reducedHealing = (total_healing * (1-game->power_reduction));
     for(int i = 0; i < player->adj_size; i++){
         DFS(player->adj[i], hopNumber+1, game, reducedHealing, player);
@@ -92,6 +98,7 @@ int main(int argc, char const *argv[])
         return 1;
     }
     
+    //initializing the game
     Game game;
 
     game.initial_range = atoi(argv[1]);
@@ -102,11 +109,13 @@ int main(int argc, char const *argv[])
     game.best_path = malloc((game.num_jumps)*sizeof(Node *));
     game.best_healing = 0;
 
+    //initializing all of the values in the array
     game.healing = malloc((game.num_jumps)*sizeof(int));
     for (int i = 0; i < game.num_jumps; i++) {
         game.healing[i] = 0;
     }
 
+    //about to scanf all of the nodes
     int x, y, current_pp, max_pp;
     char name[101] = "";
 
@@ -114,7 +123,8 @@ int main(int argc, char const *argv[])
     Node *prev = NULL;
 
     while(scanf("%d %d %d %d %s", &x, &y, &current_pp, &max_pp, name) == 5){
-
+        
+        //new players initialized
         Node *player = (Node *)malloc(sizeof(Node));
 
         player->x = x;
@@ -141,29 +151,37 @@ int main(int argc, char const *argv[])
         players[i]->prev = NULL;
     }
 
+    //determining if the nodes are connected and within initial range of 
+    //urgosa so that we know the adj lists necessary
     for(int i=0; i < size; i++){
         int x = players[i]->x;
         int y = players[i]->y;
 
         int initialRangeSquared = game.initial_range*game.initial_range;
 
+        //this for loop is to determine the size of the adj_list because the size MUST
+        //be known because we must know the size before allocating memory and once
+        //memory has been allocated we cannot add more to it
         for(int j=0; j < size; j++){
             int xDiff = (players[j]->x - x);
             int yDiff = (players[j]->y - y);
 
             int hopsNeeded = (xDiff*xDiff) + (yDiff*yDiff);
 
+            //is it a neighbor of the current node
             if((hopsNeeded <= (game.jump_range*game.jump_range)) && strcmp(players[j]->name, players[i]->name) != 0){
                 players[i]->adj_size++;
             }
-
+            //is the current node within initial range of urgosa? can we start the initial DFS at this node?
             if(hopsNeeded <= initialRangeSquared && strcmp("Urgosa_the_Healing_Shaman", players[i]->name)==0){
                 players[j]->withinInitialRange = 1;
             }
         }
 
+        //allocating the space for all of the players
         players[i]->adj = malloc((players[i]->adj_size)*sizeof(Node *));
 
+        //putting each of these players into corresponding place in the array
         int neighborsFound = 0;
         for(int j=0; j < size; j++){
             int xDiff = (players[j]->x - x);
@@ -177,18 +195,21 @@ int main(int argc, char const *argv[])
         }
     }
 
+    //making DFS call
     for(int i = 0; i < size; i++){
         if(players[i]->withinInitialRange == 1){
             DFS(players[i], 1, &game, game.initial_power, players[i]->prev);
         }
     }
 
+    //creating output
     for(int i = 0; i < game.best_path_length; i++){
         printf("%s %i\n", game.best_path[i]->name, game.healing[i]);
     }
 
     printf("Total_Healing %i\n", game.best_healing);
 
+    //freeing all of the variables
     for (int i = 0; i < size; i++){
         free(players[i]->adj);
         free(players[i]->name);
@@ -196,9 +217,9 @@ int main(int argc, char const *argv[])
     }
 
     free(game.best_path);
-    game.best_path = NULL;
+    // game.best_path = NULL;
     free(game.healing);
-    game.healing = NULL;
+    // game.healing = NULL;
 
     return 0;
 }
